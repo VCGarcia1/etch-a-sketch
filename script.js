9064a03c3d00a5ffa62eb1e1f051adf814cc436e
Veronica <vcg2106@gmail.com>
2022-07-26 10:51:39 -0400

    User inputs choice for size of grid

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const container = document.querySelector('#container');
 const rows = document.createElement(`div`);
 rows.className += ('rows')
-let rowLines = 16;
+let rowLines=0;
 
-for (i=1; i<=rowLines; i++){
-    const row = document.createElement(`div`);
-    row.className += ('row');
-    for (j=1; j<=rowLines; j++){
-        const square = document.createElement(`div`);
-        square.className += ('square');
-        row.appendChild(square);
-        square.addEventListener('mouseover', function(event) {
-            square.style.backgroundColor= 'black';
-            console.log('hover')
-        })
+function createGrid(rowLines) {
+    for (i=1; i<=rowLines; i++){
+        const row = document.createElement(`div`);
+        row.className += ('row');
+        for (j=1; j<=rowLines; j++){
+            const square = document.createElement(`div`);
+            square.className += ('square');
+            row.appendChild(square);
+            square.addEventListener('mouseover', function(event) {
+                square.style.backgroundColor= 'black';
+                console.log('hover')
+            })
+        }
+        rows.appendChild(row)
+
     }
-    rows.appendChild(row)
-
+    container.appendChild(rows)
 }
-container.appendChild(rows)
+
+while (rowLines<1 || rowLines>100){
+    rowLines = prompt('What size grid would you like (1-100)')
+}
+
+createGrid(rowLines)
+
+
+
+
